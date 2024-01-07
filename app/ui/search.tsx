9fdb773744b690df2073e674bffd2658fbbffeb9
Lucas Maniero <lucasfomaniero@gmail.com>
2024-01-07 00:57:56 -0300

    async loading on Page Components

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,14 +1,29 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useEffect, useState } from 'react';
+import useDebounce from '../lib/useDebounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const [search, setSearch] = useState('')
+  const debouncedSearch = useDebounce(search, 500)
+
+  useEffect(() => {
+    if(debouncedSearch) {
+      console.log(debouncedSearch)
+    }
+   
+  }, [debouncedSearch])
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
+        onChange={(e) => {
+          setSearch(e.target.value)
+        }}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
       />
