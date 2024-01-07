c3446dc980e9f9b19fa8d060532a12cc1f0f03b2
Lucas Maniero <lucasfomaniero@gmail.com>
2024-01-07 12:44:57 -0300

    Speed meter added

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,11 @@
 import SideNav from "../ui/dashboard/sidenav";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
       <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
         <div className="w-full flex-none md:w-64">
+          <SpeedInsights />
           <SideNav />
         </div>
         <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
